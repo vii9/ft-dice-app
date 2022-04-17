055c564f0d753d56a4ec1125893463831bd4f914
ag.thanhz <ag.thanhz@gmail.com>
2022-04-17 21:05:36 +0700

    switch to stateFull

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,49 @@ void main() {
   ));
 }
 
+class DiceepPage extends StatefulWidget {
+  const DiceepPage({Key? key}) : super(key: key);
+
+  @override
+  State<DiceepPage> createState() => _DiceepPageState();
+}
+
+class _DiceepPageState extends State<DiceepPage> {
+  int leftDiceNumber = 3;
+  @override
+  Widget build(BuildContext context) {
+    int rightDiceNumber = 5;
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 4;
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('right button got pressed!');
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+/*
+  //#stateFull => data change
+  //#stateLess => static layout
+
 class DiceepPage extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -44,3 +87,4 @@ class DiceepPage extends StatelessWidget {
     );
   }
 }
+*/
